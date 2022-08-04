df7e215d417e6e942f040c6264c7d07eab2a07e2
veeraboinajashwanth <94590273+veeraboinajashwanth@users.noreply.github.com>
2022-08-04 16:16:58 +0530

    04/08/22 lenth of string

---
+++ length_of _string_code.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+ 
+int main()
+{
+  char Str[100];
+  int lg;
+ 
+  printf("\n Please Enter any String \n");
+  gets (Str);
+ 
+  lg = strlen(Str);
+ 
+  printf("Length = %d\n", lg);
+ 
+  return 0;
+}
