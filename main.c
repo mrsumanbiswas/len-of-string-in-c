29ccdc4df49899f72cec35fb56b718412452bee8
mrsumanbiswas <mrsumanbiswas0@gmail.com>
2022-08-04 23:46:47 +0530

    Code Optimization is done! ** Use Less possible includes ** char may be longer so take it size of 1000 ** I changed the file name because the code retuning error code 1

---
+++ main.c
@@ -0,0 +1,13 @@
+// C program to find the length of string
+#include <stdio.h>
+int main()
+{
+    char String[1000];
+    int string_lenght;
+    printf("Enter a string and check it's lenght\n");
+    scanf("%s", String);
+    for (; String[string_lenght] != '\0'; string_lenght++)
+        ;
+    printf("The lenght of the string is %d\n", string_lenght);
+    return 0;
+}
